211c095d5ce3683bca6926d57f131888e57804a8
Alvard <user@Alvards-MacBook-Pro.local>
2025-03-06 04:03:31 +0400

    update profil  popover

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,55 +1,62 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: `https://ugogo-auhdbad8drdma7f6.canadacentral-01.azurewebsites.net`
+  baseURL: `https://ugogo-auhdbad8drdma7f6.canadacentral-01.azurewebsites.net`,
 });
 
-api.interceptors.request.use((request: any) => {
-        const accessToken = localStorage.getItem('access');
+api.interceptors.request.use(
+  (request: any) => {
+    const accessToken = localStorage.getItem("access");
 
-        return {
-            ...request,
-            headers: {
-                ...(accessToken !== null && {Authorization: `Bearer ${accessToken}`}),
-                ...request.headers,
-                'Access-Control-Allow-Origin': '*'
-            },
-        };
-    },
-    (error) => {
-        return Promise.reject(error);
-    }
+    return {
+      ...request,
+      headers: {
+        ...(accessToken !== null && { Authorization: `Bearer ${accessToken}` }),
+        ...request.headers,
+        "Access-Control-Allow-Origin": "*",
+      },
+    };
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 api.interceptors.response.use(
-    (response: any) => response, // Pass successful responses as is
-    async (error: any) => {
-        const originalRequest = error.config;
+  (response: any) => response, // Pass successful responses as is
+  async (error: any) => {
+    const originalRequest = error.config;
 
-        // If unauthorized and it's the first retry, attempt refresh
-        if (error.response?.status === 401 && !originalRequest._retry) {
-            originalRequest._retry = true; // Prevent infinite loop
+    // If unauthorized and it's the first retry, attempt refresh
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true; // Prevent infinite loop
 
-            try {
-                const refreshToken = localStorage.getItem("refresh");
-                if (!refreshToken) throw new Error("No refresh token available");
+      try {
+        const refreshToken = localStorage.getItem("refresh");
+        if (!refreshToken) throw new Error("No refresh token available");
 
-                // Request new access token using refresh token
-                const { data } = await api.post("/users/token/refresh", { refresh: refreshToken });
-                // Store new tokens
-                localStorage.setItem("access", data.access);
-                localStorage.setItem("refresh", data.refresh);
+        // Request new access token using refresh token
+        const { data } = await api.post("/users/token/refresh/", {
+          refresh: refreshToken,
+        });
+        // Store new tokens
+        localStorage.setItem("access", data.access);
+        localStorage.setItem("refresh", data.refresh);
 
-                // Retry the failed request with new token
-                originalRequest.headers.Authorization = `Bearer ${data.access}`;
-                return api(originalRequest);
-            } catch (err) {
-                console.error("Refresh token failed", err);
-                localStorage.removeItem('token');
-                window.location.href = "/login";
-            }
-        }
+        // Retry the failed request with new token
+        originalRequest.headers.Authorization = `Bearer ${data.access}`;
+        return api(originalRequest);
+      } catch (err) {
+        console.error("Refresh token failed", err);
+        // localStorage.removeItem('token');
+        localStorage.removeItem("access");
+        localStorage.removeItem("refresh");
 
-        return Promise.reject(error);
+        window.location.href = "/login";
+      }
     }
+
+    return Promise.reject(error);
+  }
 );
+
--- src/components/profilePopover/profilePopover.tsx
+++ src/components/profilePopover/profilePopover.tsx
@@ -1,73 +1,153 @@
-import {Divider} from "../divider/Divider";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Divider } from "../divider/Divider";
 import avatar from "../../assets/images/avatar.svg";
-import settingsIcon from "../../assets/icons/settings.svg";
-import { logout } from "../../api/route";
-import {useNavigate} from "react-router-dom";
+import flightsIcon from "../../assets/icons/flights.svg";
+import flightsHoverIcon from "../../assets/icons/flightsHover.svg";
+import requestsIcon from "../../assets/icons/requests.svg";
+import requestsHoverIcon from "../../assets/icons/requestsHover.svg";
+import itemsIcon from "../../assets/icons/items.svg";
+import itemHoverIcon from "../../assets/icons/itemsHover.svg";
+import accountIcon from "../../assets/icons/account_circle.svg";
+import accountHoverIcon from "../../assets/icons/account_circle_hover.svg";
+import logoutIcon from "../../assets/icons/logout.svg";
+import logoutHoverIcon from "../../assets/icons/logoutHover.svg";
+import { logout, getUserDetails } from "../../api/route";
 
-export const ProfilePopover = () => {
-    const navigate = useNavigate();
-
-    const handleLogout = async () => {
-        const refreshToken = localStorage.getItem('refresh');
-        if (!refreshToken) {
-            return
-        }
-
-        try {
-            await logout(refreshToken);
-            localStorage.removeItem('access')
-            localStorage.removeItem('refresh')
-            navigate('/login')
-        } catch (e) {
-            console.error(e)
-        }
-
-    }
-
-    return (
-        <div className="flex flex-col border border-solid border-[#D5D7DA] rounded-[1rem] absolute top-[70%] left-[90%] min-w-[300px] translate-x-[-50%] bg-[#fff]">
-            <div className="profile__avatar px-[2.1rem] py-[1.6rem]">
-                <div className="profile__avatar__image">
-                    <img src={avatar} alt="Avatar" className="profile__avatar__image"/>
-                </div>
-                <div className="profile__avatar__details">
-                    <div className="profile__avatar__details__name">
-                        John Doe
-                    </div>
-                    <div className="profile__avatar__details__rate">
-                        <span className="profile__avatar__details__rate__value">
-                            John.doe@gmail.com
-                        </span>
-                    </div>
-                </div>
-            </div>
-            <Divider appearance={'neutral'} size={'small'} className="mt-[1.6rem]" />
-            <div className="flex flex-col gap-[.8rem] px-[2.1rem] py-[1.6rem]">
-                <div className="flex items-center gap-[.8rem] cursor-pointer">
-                    <div className="flex">
-                        <img src={settingsIcon} alt="settings icon"/>
-                    </div>
-                    <span className="text-[1.4rem] text-[#808080]">
-                        My account
-                    </span>
-                </div>
-                <div className="flex items-center gap-[.8rem] cursor-pointer">
-                    <div className="flex">
-                        <img src={settingsIcon} alt="settings icon"/>
-                    </div>
-                    <span className="text-[1.4rem] text-[#808080]">
-                        Settings
-                    </span>
-                </div>
-                <div className="flex items-center gap-[.8rem] cursor-pointer">
-                    <div className="flex">
-                        <img src={settingsIcon} alt="settings icon"/>
-                    </div>
-                    <span className="text-[1.4rem] text-[#808080]" onClick={handleLogout}>
-                        Logout
-                    </span>
-                </div>
-            </div>
-        </div>
-    )
+interface User {
+  name: string;
+  email: string;
 }
+
+export const ProfilePopover: React.FC = () => {
+  const navigate = useNavigate();
+
+  const [requestsIconState, setRequestsIcon] = useState(requestsIcon);
+  const [itemsIconState, setItemsIcon] = useState(itemsIcon);
+  const [flightsIconState, setFlightsIconState] = useState(flightsIcon);
+  const [accountIconState, setAccountIcon] = useState(accountIcon);
+  const [logoutIconState, setLogoutIcon] = useState(logoutIcon);
+
+  const [user, setUser] = useState<User>({ name: "NULL", email: "NULL" });
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const userData = await getUserDetails();
+        if (userData) {
+          setUser({ name: userData.full_name, email: userData.email });
+        }
+      } catch (error) {
+        console.error("Error fetching user details:", error);
+      }
+    };
+    fetchUser();
+  }, []);
+
+  const handleLogout = async () => {
+    try {
+      const refreshToken = localStorage.getItem("refresh");
+      if (!refreshToken) {
+        console.log("No refresh token found. Logging out...");
+        localStorage.removeItem("access");
+        localStorage.removeItem("refresh");
+        navigate("/login");
+        return;
+      }
+
+      await logout(refreshToken);
+      localStorage.removeItem("access");
+      localStorage.removeItem("refresh");
+      navigate("/login");
+    } catch (error) {
+      console.error("Logout failed", error);
+      localStorage.removeItem("access");
+      localStorage.removeItem("refresh");
+      navigate("/login");
+    }
+  };
+
+  return (
+    <div className="flex flex-col border border-solid border-[#D5D7DA] rounded-[1rem] absolute top-[70%] left-[90%] min-w-[300px] translate-x-[-50%] bg-[#fff]">
+      <div className="profile__avatar px-[2.1rem] py-[1.6rem]">
+        <div className="profile__avatar__image">
+          <img src={avatar} alt="Avatar" className="profile__avatar__image" />
+        </div>
+        <div className="profile__avatar__details">
+          <div className="profile__avatar__details__name">{user.name}</div>
+          <div className="profile__avatar__details__rate">
+            <span className="profile__avatar__details__rate__value">
+              {user.email}
+            </span>
+          </div>
+        </div>
+      </div>
+      <Divider appearance="neutral" size="small" className="mt-[1.6rem]" />
+      <div className="flex flex-col gap-[.8rem] px-[2.1rem] py-[1.6rem] ">
+
+      <div
+          className="flex items-center gap-[.8rem] cursor-pointer"
+          onClick={() => navigate("/add-profile-info")}
+          onMouseEnter={() => setAccountIcon(accountHoverIcon)}
+          onMouseLeave={() => setAccountIcon(accountIcon)}
+        >
+          <img src={accountIconState} alt="Account Icon" className="w-[20px] h-[20px]" />
+          <span className="text-[1.4rem] text-[#808080] hover:text-[#F9A34B]">
+            My Account
+          </span>
+        </div>
+
+        <div
+          className="flex items-center gap-[.8rem] cursor-pointer"
+          onClick={() => navigate("/requests")}
+          onMouseEnter={() => setRequestsIcon(requestsHoverIcon)}
+          onMouseLeave={() => setRequestsIcon(requestsIcon)}
+        >
+          <img src={requestsIconState} alt="Requests Icon" className="w-[20px] h-[20px]" />
+          <span className="text-[1.4rem] text-[#808080] hover:text-[#F9A34B]">
+            My Requests
+          </span>
+        </div>
+
+        <div
+          className="flex items-center gap-[.8rem] cursor-pointer"
+          onClick={() => navigate("/items")}
+          onMouseEnter={() => setItemsIcon(itemHoverIcon)}
+          onMouseLeave={() => setItemsIcon(itemsIcon)}
+        >
+          <img src={itemsIconState} alt="Items Icon" className="w-[20px] h-[20px]" />
+          <span className="text-[1.4rem] text-[#808080] hover:text-[#F9A34B]">
+            My Items
+          </span>
+        </div>
+
+        <div
+          className="flex items-center gap-[.8rem] cursor-pointer"
+          onClick={() => navigate("/offers")}
+          onMouseEnter={() => setFlightsIconState(flightsHoverIcon)}
+          onMouseLeave={() => setFlightsIconState(flightsIcon)}
+        >
+          <img src={flightsIconState} alt="Flights Icon" className="w-[20px] h-[20px]" />
+          <span className="text-[1.4rem] text-[#808080] hover:text-[#F9A34B]">
+            My Flights
+          </span>
+        </div>
+
+
+        <Divider appearance="neutral" size="small" className="mt-[1.6rem]" />
+
+        <div
+          className="flex items-center gap-[.8rem] cursor-pointer"
+          onClick={handleLogout}
+          onMouseEnter={() => setLogoutIcon(logoutHoverIcon)}
+          onMouseLeave={() => setLogoutIcon(logoutIcon)}
+        >
+          <img src={logoutIconState} alt="Logout Icon" className="w-[20px] h-[20px]" />
+          <span className="text-[1.4rem] text-[#808080] hover:text-[#F9A34B]">
+            Logout
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
