dbba5a2bfe16c0ec90fc83aae8e27af3fd104d47
Areg <areg@espoc.com>
2025-03-29 20:49:28 +0400

    bug fixes related to the multiple image uploads and offer card buttons

--- src/pages/offers/Offers.tsx
+++ src/pages/offers/Offers.tsx
@@ -28,38 +28,14 @@ export const Offers = () => {
       <div className="flex flex-col gap-[6rem] w-full">
         <h3 className="text-[2rem] font-medium">My offers</h3>
         <div className="grid grid-cols-3 gap-[5.7rem] justify-items-center">
-          <OfferCard
-            primaryButtonText={"Accept"}
-            secondaryButtonText={"Decline"}
-          />
-          <OfferCard
-            primaryButtonText={"Accept"}
-            secondaryButtonText={"Decline"}
-          />
-          <OfferCard
-            primaryButtonText={"Accept"}
-            secondaryButtonText={"Decline"}
-          />
-          <OfferCard
-            primaryButtonText={"Accept"}
-            secondaryButtonText={"Decline"}
-          />
-          <OfferCard
-            primaryButtonText={"Accept"}
-            secondaryButtonText={"Decline"}
-          />
-          <OfferCard
-            primaryButtonText={"Accept"}
-            secondaryButtonText={"Decline"}
-          />
-          <OfferCard
-            primaryButtonText={"Accept"}
-            secondaryButtonText={"Decline"}
-          />
-          <OfferCard
-            primaryButtonText={"Accept"}
-            secondaryButtonText={"Decline"}
-          />
+          <OfferCard secondaryButtonText={'Delete'} />
+          <OfferCard secondaryButtonText={'Delete'} />
+          <OfferCard secondaryButtonText={'Delete'} />
+          <OfferCard secondaryButtonText={'Delete'} />
+          <OfferCard secondaryButtonText={'Delete'} />
+          <OfferCard secondaryButtonText={'Delete'} />
+          <OfferCard secondaryButtonText={'Delete'} />
+          <OfferCard secondaryButtonText={'Delete'} />
         </div>
         <div className="flex justify-end gap-[4rem]">
           <Button
