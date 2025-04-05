590da460649fe1f874dda496bee95bd9ce979e4a
Alvard Khchoyan <alvardkhchoyan@bytessphere.onmicrosoft.com>
2025-04-05 18:58:00 +0000

    Merged PR 33: added error highlighting for required fields in PostOffer

--- src/pages/offers/Offers.tsx
+++ src/pages/offers/Offers.tsx
@@ -4,38 +4,35 @@ import { useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 export const Offers = () => {
-    const location = useLocation();
-    const [notificationMessage, setNotificationMessage] = useState(
-      location.state?.notification || ""
-    );
-  
-    useEffect(() => {
-      if (notificationMessage) {
-        const timer = setTimeout(() => {
-          setNotificationMessage(""); // Hide notification after 2 seconds
-        }, 2000);
-        return () => clearTimeout(timer); // Cleanup timeout on component unmount
-      }
-    }, [notificationMessage]);
-  
+  const location = useLocation();
+  const [notificationMessage, setNotificationMessage] = useState(
+    location.state?.notification || ""
+  );
+
+  useEffect(() => {
+    if (notificationMessage) {
+      const timer = setTimeout(() => {
+        setNotificationMessage(""); // Hide notification after 2 seconds
+      }, 2000);
+      return () => clearTimeout(timer); // Cleanup timeout on component unmount
+    }
+  }, [notificationMessage]);
+
   return (
     <>
       {notificationMessage && (
-  <div className="fixed top-4 left-1/2 transform -translate-x-1/2 bg-green-500 text-white px-6 py-3 rounded-md shadow-lg text-center animate-fade-out">
+        <div className="fixed top-4 left-1/2 transform -translate-x-1/2 bg-green-500 text-white px-6 py-3 rounded-md shadow-lg text-center animate-fade-out">
           <p>{notificationMessage}</p>
         </div>
       )}
       <div className="flex flex-col gap-[6rem] w-full">
         <h3 className="text-[2rem] font-medium">My offers</h3>
         <div className="grid grid-cols-3 gap-[5.7rem] justify-items-center">
-          <OfferCard secondaryButtonText={'Delete'} />
-          <OfferCard secondaryButtonText={'Delete'} />
-          <OfferCard secondaryButtonText={'Delete'} />
-          <OfferCard secondaryButtonText={'Delete'} />
-          <OfferCard secondaryButtonText={'Delete'} />
-          <OfferCard secondaryButtonText={'Delete'} />
-          <OfferCard secondaryButtonText={'Delete'} />
-          <OfferCard secondaryButtonText={'Delete'} />
+          <OfferCard
+            primaryButtonText={"Accept"}
+            secondaryButtonText={"Decline"}
+            offerData={{}}
+          />
         </div>
         <div className="flex justify-end gap-[4rem]">
           <Button
