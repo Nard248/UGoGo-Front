5dc81f98fbe71c750f9bc2defd43c6dd721019ef
narekmeloyan <narek@wireless2020.com>
2025-08-18 00:08:37 +0400

    API Fix

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: `https://ugogo-auhdbad8drdma7f6.canadacentral-01.azurewebsites.net`,
-  baseURL: `http://127.0.0.1:8000/`,
+  baseURL: `https://ugogo-auhdbad8drdma7f6.canadacentral-01.azurewebsites.net`,
+  // baseURL: `http://127.0.0.1:8000/`,
 });
 
 api.interceptors.request.use(
